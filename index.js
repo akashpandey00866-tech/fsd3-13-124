9b24cb00f78b909748eac47c7ad93798a924fff2
Akash Pandey <akashpandey00866@gmail.com>
2026-07-22 11:12:14 +0530

    Add functions for greeting and summation with example usage

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hellow Node JS");
+function sayHello(name){
+    console.log('welcom to ${name}');
+}
+function sum (a,b){
+    return a+b;
+}
+sayHello('akash pandey');
+console.log(sum(2,5));
+function sum(a , b){
+    a=5;
+    b=8;
+    return a+b;
+}
+
+
