57733e753826b6a597033d0e8b6ca43bd778746c
Akash Pandey <akashpandey00866@gmail.com>
2026-09-23 12:09:46 +0530

    date 23-9-2026

--- lab3/products.js
+++ lab3/products.js
@@ -1,24 +1,45 @@
 const products = [
-    {id:1, name: 'marker' ,qty:100,price:103},
-    {id:2, name: 'duster' ,qty:50,price:50},
-]
-let nextID = 3;
-   export const getAllproducts =()=>{//arrow fuction
-    return products;
-}
+  { id: 1, name: "marker", qty: 100, price: 15 },
+  { id: 2, name: "duster", qty: 50, price: 10 },
+];
 
- export const AddProducts = (item) =>{
-    item.id = nextID;
-    nextID++;
-    products.push(item);
-    return item;
+let nextId = 3;
 
+export const getAllProducts = () => {
+  return products;
+};
 
- };
- export const deleteProduct =(pid)=>{
-    const item =products.findIndex((prd)=>prd,id===pid);
-    if(item ==-1)return false;
-    products.splice(item, 1);
-    console.log("products remaining:",products);
-    return true;
- };
+export const addProduct = (item) => {
+  item.id = nextId;
+  nextId++;
+  products.push(item);
+  return item;
+};
+
+export const deleteProduct = (pid) => {
+  const item = products.findIndex((prd) => prd.id === pid);
+  if (item == -1) return false;
+  products.splice(item, 1);
+  console.log("products remaining:", products);
+  return true;
+};
+
+export const updateProduct = (pid, updateItem) => {
+  const index = products.findIndex((prd) => prd.id === pid);
+
+  if (index == -1) {
+    return false;
+  }
+  updateItem.id = pid;
+  products[index] = updateItem;
+  return updateItem;
+};
+
+export const getProductById = (pid) => {
+  const index = products.findIndex((prd) => prd.id === pid);
+
+  if (index == -1) {
+    return false;
+  }
+  return products[index];
+};
